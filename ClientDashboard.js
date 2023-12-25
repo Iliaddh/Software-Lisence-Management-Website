664730d6bcad55deb9d49a46e10959b62ba8af4f
Iliad Hemmatjoo <124857326+Iliaddh@users.noreply.github.com>
2023-12-25 16:39:51 -0500

    Add files via upload

---
+++ ClientDashboard.js
@@ -0,0 +1,267 @@
+import { auth , signOut , database, set } from "./firebaseConfig.js"
+import { ref, get , child , update } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+
+
+const logOutBtn = document.getElementById("logOut")
+
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in
+        console.log('User is signed in:', user);
+    } else {
+        // User is signed out
+        console.log('User is signed out');
+    }
+});
+
+logOutBtn.addEventListener('click' , async () => {
+    try{
+        await signOut(auth);
+        console.log("sign out successful");
+        window.location.href = 'mainPage.html';
+    }catch (error) {
+        console.error('Error signing out: ' , error.message);
+    }
+});
+
+
+var softwareLicenses;
+var softwareKeys;
+
+async function getLicenses(){
+    const softwareRef = ref(database, 'softwareLicenses/');
+
+    try {
+        const snapshot = await get(child(softwareRef, '/'));
+        if (snapshot.val()) {
+            softwareLicenses = Object.values(snapshot.val());
+            softwareKeys = Object.keys(snapshot.val());
+        } else {
+            console.log('No software found in the database.');
+        }
+    } catch (error) {
+        console.error('Error retrieving software:', error.message);
+    }
+}
+
+async function displayLicenses() {
+
+    await getLicenses();
+
+    const licenseTableBody = document.getElementById("license-table-body");
+
+    licenseTableBody.innerHTML = "";
+    softwareLicenses.forEach((license , index) => {
+        if(license.user == auth.currentUser.uid){
+            const expiration = (2023 + Number(softwareLicenses[index].duration)).toString();
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${license.softwareName}</td>
+                <td>${softwareKeys[index]}</td>
+                <td>${expiration}</td>
+                <td><button class="delete-button" data-key="${softwareKeys[index]}">Delete</button><button class="renew-button">Renew</button></td>      
+            `;
+
+            licenseTableBody.appendChild(row);
+        }
+    });
+}
+
+
+document.getElementById("license-table-body").addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-button")) {
+        const licenseKey = event.target.getAttribute('data-key');
+        deleteLicenseHandler(licenseKey);
+    }
+});
+
+
+async function deleteLicenseHandler(key){
+    await getLicenses();
+
+    const updates = {};
+    updates[`/softwareLicenses/${key}/user`] = "";
+    update(ref(database), updates);
+    const row = document.querySelector(`[data-key="${key}"]`);
+    if(row){
+        row.parentElement.parentElement.remove();
+    }
+    
+}
+
+// Define a function to add a license to the table
+async function addLicense(event) {
+    event.preventDefault(); // Prevent the form from submitting and refreshing the page
+
+    const softwareRef = ref(database, 'softwareLicenses');
+
+    const name = document.getElementById("name").value;
+    const key = document.getElementById("key").value;
+    const duration = document.getElementById("duration").value;
+
+
+    if (name && key && duration) {
+        const newLicense = { 
+            softwareName: name,
+            duration: duration,
+            user: auth.currentUser.uid,
+            cost: 1000,
+            userEntry: true
+         };
+
+        set(child(softwareRef , key) , newLicense)
+            .then(() => {
+                console.log('Software added successfully');
+            })
+            .catch((error) => {
+                console.error('Error adding software:', error.message);
+            });
+
+        showLicenseForm();
+        displayLicenses();
+    }
+}
+
+// Event listener for the form submission
+document.getElementById("add-license-form").addEventListener("submit", addLicense);
+
+function showLicenseForm(){
+    if(document.getElementById('add-license-form').style.display == "inline-block")
+        document.getElementById("add-license-form").style.display = "none";
+    document.getElementById("add-license-form").style.display = "inline-block";
+}
+document.getElementById("add-license-button").addEventListener("click", showLicenseForm);
+
+
+
+async function displayAvailableLicenses() {
+    await getLicenses();
+
+    const storeLicensesDiv = document.getElementById("store-licenses");
+
+    storeLicensesDiv.innerHTML = "";
+    softwareLicenses.forEach((license, index) => {
+        // Check if the license has no user assigned
+        if (!license.user && !license.userEntry) {
+            const licenseItem = document.createElement("div");
+            licenseItem.classList.add("store-license-item");
+            licenseItem.innerHTML = `
+                <div class="license-details">
+                    <h3 id>${license.softwareName}</h3>
+                    <p>Price: $${license.cost}</p>
+                    <p>Duration: ${license.duration}</p>
+                </div>
+                <button class="purchase-button" data-name="${license.softwareName}">Purchase</button>
+            `;
+
+            storeLicensesDiv.appendChild(licenseItem);
+        }
+    });
+}
+
+
+
+
+async function moveLicenseToUserTable(name) {
+    await getLicenses();
+    const index = softwareLicenses.findIndex((license) => license.softwareName === name)
+    const key = softwareKeys[index];
+    const expiration = (2023 + Number(softwareLicenses[index].duration)).toString();
+
+    console.log(softwareKeys);
+    const licenseToAdd = {
+        name: softwareLicenses[index].softwareName,
+        key: key,
+        expiration: expiration,
+    }
+    softwareLicenses.splice(index, 1);
+
+    const updates = {};
+    updates[`/softwareLicenses/${key}/user`] = auth.currentUser.uid;
+    update(ref(database), updates);
+
+    console.log(softwareLicenses); 
+
+    displayLicenses();
+    displayAvailableLicenses();
+}
+
+// Event listener for the "Purchase" button clicks
+document.getElementById("store-licenses").addEventListener("click", (event) => {
+    if (event.target.classList.contains("purchase-button")) {
+        const name = event.target.getAttribute("data-name");
+        moveLicenseToUserTable(name);
+    }
+});
+
+
+window.onload = () => {
+    displayLicenses();
+    displayAvailableLicenses();
+}
+
+
+// Event listener for the form submission
+document.getElementById("add-license-form").addEventListener("submit", addLicense);
+
+document.getElementById("license-table-body").addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-button")) {
+        const licenseKey = event.target.getAttribute('data-key');
+        deleteLicenseHandler(licenseKey);
+    } else if (event.target.classList.contains("renew-button")) {
+        const licenseKey = event.target.parentElement.querySelector(".delete-button").getAttribute('data-key');
+        renewLicenseHandler(licenseKey);
+    }
+});
+
+
+async function renewLicenseHandler(key) {
+    await getLicenses();
+
+
+    const yearsToRenew = prompt("Enter the number of years to renew:");
+
+    if (yearsToRenew !== null && !isNaN(yearsToRenew) && parseInt(yearsToRenew) > 0) {
+        const updates = {};
+        const index = softwareKeys.findIndex((license) => license === key);
+
+        console.log(index)
+
+        softwareLicenses[index].duration = parseInt(softwareLicenses[index].duration) + parseInt(yearsToRenew);
+        updates[`/softwareLicenses/${key}/duration`] = softwareLicenses[index].duration;
+        update(ref(database) , updates);
+
+
+        console.log("Updates:", updates);
+
+        displayLicenses();
+
+    } else {
+        alert("Invalid input. Please enter a valid number of years.");
+    }
+}
+
+// ClientDashboard.js
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('dropdownButton').addEventListener('click', myFunction);
+});
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
---
+++ LogIn.js
@@ -0,0 +1,265 @@
+
+
+
+const LogInButton = document.getElementById("signinBtn");
+const signInForm = document.getElementById("signinForm");
+const signUpForm = document.getElementById("signupForm");
+const toggleButton = document.getElementById("toggleButton");
+const toggleButton2 = document.getElementById("toggleButton2");
+const signUpBtn = document.getElementById("signUpBtn");
+
+
+
+const userPass = {
+    userName: "hello@gmail.com",
+    userName2: "client@gmail.com",
+    password: "123456",
+    password2: "7890",
+    firstName: "John",
+    lastName: "Doe",
+    firstName2: "Mike",
+    lastName2: "Dillon",
+}
+
+import { database, ref ,set , auth, createUserWithEmailAndPassword, signInWithEmailAndPassword , updateProfile} from "./firebaseConfig.js";
+const passwordInput = document.getElementById("signUpPassword").value;
+// Sign up
+signUpBtn.addEventListener('click' , async () => {
+    
+
+    event.preventDefault();
+    
+    // get values from form
+    const user = {
+        fName: document.getElementById('firstName').value,
+        lName: document.getElementById('lastName').value,
+        email: document.getElementById('signUpEmail').value,
+        provider: document.querySelector('input[name="provider"]:checked').value
+
+    };
+
+    
+    const password = document.getElementById('signUpPassword').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    const passwordError =document.getElementById("passowrdError");
+    const confirmPasswordError =document.getElementById("confirmPassowrdError");
+    // if(password.length < 8){
+    //     passwordError.style.display = "block";
+    //     function myFunction() {
+    //         passwordError.style.display = "none";
+    //       }
+          
+    //       // Set a timeout for 2000 milliseconds (2 seconds)
+    //       setTimeout(myFunction, 2000);
+    // }else{
+    //     if(password != confirmPassword){
+    //         confirmPasswordError.style.display = "block";
+    //         function myFunction() {
+    //             confirmPasswordError.style.display = "none";
+    //           }
+              
+    //           // Set a timeout for 2000 milliseconds (2 seconds)
+    //           setTimeout(myFunction, 2000);
+    //     }
+    // }
+//check inputs
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const emailInput = document.getElementById('signUpEmail').value;
+
+    // Display a message based on the result
+    const firstNameInput = document.getElementById("FirstInputEmpty");
+    const lastNameInput = document.getElementById("lastInputEmpty");
+    const emptyEmailError = document.getElementById("emptyEmailError");
+    const invalidEmailError = document.getElementById("emailError");
+
+
+    // Regular expression for a simple email validation
+    // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // const emailRegex = /r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"/
+    // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // const gmailRegex = /^[^\s@]+@gmail\.com$/;
+    const emailRegex = /^[^\s@]+@gmail\.com$/;
+
+
+
+
+
+    if (firstName === '') {
+        
+        firstNameInput.style.display = "block";
+        function FirstInputEmptyFunction(){
+            firstNameInput.style.display = "none";
+        }
+
+        setTimeout(FirstInputEmptyFunction, 2000);
+    } else if(lastName === ''){
+        lastNameInput.style.display = "block";
+        function LastInputEmptyFunction(){
+            lastNameInput.style.display = "none";
+        }
+
+        setTimeout(LastInputEmptyFunction, 2000);
+    }else if(emailInput === ''){
+        emptyEmailError.style.display = "block";
+        function LastInputEmptyFunction(){
+            emptyEmailError.style.display = "none";
+        }
+
+        setTimeout(LastInputEmptyFunction, 2000);
+    }
+    
+    if(emailRegex.test(emailInput) && password.length >= 6 && password == confirmPassword){
+    try {
+        
+            console.log("hi")
+        const userCredential = await createUserWithEmailAndPassword(auth, user.email, password);
+        // User creation successful
+        console.log('User created successfully:', userCredential.user);
+
+        await updateProfile(userCredential.user, {
+            displayName: `${user.fName} ${user.lName}`
+        });
+
+        // Add user to Database
+        await set(ref(database , `users/${userCredential.user.uid}`) , {
+            firstName: user.fName,
+            lastName: user.lName,
+            email: user.email,
+            provider: user.provider === 'true'
+        });
+
+        window.location.href = "mainPage.html";
+    
+
+    } catch (error) {
+        // Handle errors
+        console.error('Error creating user:', error.code, error.message);
+    }
+
+
+    // Check if user is signed in
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in
+            console.log('User is signed in:', user);
+        } else {
+            // User is signed out
+            console.log('User is signed out');
+        }
+    });
+}else if(!(emailRegex.test(emailInput))){
+    invalidEmailError.style.display = "block";
+    function LastInputEmptyFunction(){
+        invalidEmailError.style.display = "none";
+    }
+
+    setTimeout(LastInputEmptyFunction, 2000);
+}else if(password.length < 6){
+    passwordError.style.display = "block";
+    function myFunction() {
+        passwordError.style.display = "none";
+      }
+      
+      // Set a timeout for 2000 milliseconds (2 seconds)
+      setTimeout(myFunction, 2000);
+}else if(password != confirmPassword){
+        confirmPasswordError.style.display = "block";
+        function myFunction() {
+            confirmPasswordError.style.display = "none";
+          }
+          
+          // Set a timeout for 2000 milliseconds (2 seconds)
+          setTimeout(myFunction, 2000);
+    
+}
+})
+
+
+// Sign In 
+LogInButton.addEventListener('click' , async (event) => {
+
+    event.preventDefault();
+
+    // Get data from form   
+    const userEmail = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const signInError = document.getElementById("logInError");
+    // Sign in user
+    try{
+        await signInWithEmailAndPassword(auth , userEmail , password);
+    }catch(error){
+        signInError.style.display = "block";
+            function myFunction() {
+                signInError.style.display = "none";
+            }
+          
+          // Set a timeout for 2000 milliseconds (2 seconds)
+          setTimeout(myFunction, 2000);
+    }
+
+    // Check if user is signed in
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in
+            console.log('User is signed in:', user);
+            window.location.href = 'mainPage.html';
+        } else {
+            
+            // User is signed out
+            console.log('User is signed out');
+        }
+    });
+})
+
+
+
+toggleButton.addEventListener("click", () => {
+        signInForm.style.display = "none"; 
+        signUpForm.style.display = "block"; 
+
+        clearInputFields(signInForm);
+});
+
+toggleButton2.addEventListener("click", () => {
+
+    signInForm.style.display = "block"; 
+    signUpForm.style.display = "none"; 
+
+    clearInputFields(signUpForm);
+});
+
+
+function clearInputFields(form){
+    const inputFields = form.querySelectorAll("input[type='text']")
+    inputFields.forEach((input) => {
+        input.value = "";
+    });
+}
+
+// function LogInHandler(event) {
+
+//     event.preventDefault();
+//     const emailInput = document.getElementById("email").value;
+//     const passwordInput = document.getElementById("password").value;
+    
+//     if(emailInput === userPass.userName && passwordInput === userPass.password){
+//         window.location.href = "mainPage.html";
+//         ProviderisLoggedIn = true;
+//         localStorage.setItem('ProviderisLoggedIn' , ProviderisLoggedIn);
+//         console.log(ProviderisLoggedIn)
+//     }else if(emailInput === userPass.userName2 && passwordInput === userPass.password2){
+
+//         window.location.href = "mainPage.html";
+//         ClientisLoggedIn = true;
+//         localStorage.setItem('ClientisLoggedIn' , ClientisLoggedIn);
+//         console.log(ClientisLoggedIn)
+
+//     }
+//     else{
+//         alert("Password or email incorrect!")
+//     }
+// }
+
+
+// LogInButton.addEventListener("click", LogInHandler);
